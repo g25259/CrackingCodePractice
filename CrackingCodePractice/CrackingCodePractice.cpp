7d3ac515bdad25105f0c26e801e47db536dce9c3
nizi1127 <nizi1127@gmail.com>
2016-11-21 10:06:36 +0800

    Refactor how to call members in ArraysAndStrings

--- CrackingCodePractice/CrackingCodePractice.cpp
+++ CrackingCodePractice/CrackingCodePractice.cpp
@@ -2,53 +2,46 @@
 #include "stdafx.h"
 #include <iostream>
 #include "ArraysAndStrings.h"
-
+#include "CrackingCodePractice.h"
 using namespace std;
 
+void CallArraysAndStrings(int QuestionIndex)
+{
+	string str, str2;
+	string result = "";
+	ArraysAndStrings obj;
+	switch (QuestionIndex)
+	{
+		case 1:
+			cout << "Input string to determine is the string has all unique characters." << endl;
+			cin >> str;
+			result = obj.IsUniqueCharacter(str) ? "The string has all unique characters." : "The string has duplicated characters.";
+			break;
+		case 2:
+			cout << "Input string as c-style and reverse it." << endl;
+			cin >> str;
+			result.assign( obj.ReverseCStr( const_cast<char*>( str.c_str() ) ) );
+			//char* result = obj.AdvancedRreverse(const_cast<char*>(str.c_str()));
+			break;
+		case 3:
+			cout << "Remove duplicated character in the input string." << endl;
+			cin >> str;
+			result = obj.RemoveDuplicatedChar(str);
+			break;
+		case 4:
+			cout << "Is the input two strings are anagrams?" << endl;
+			cin >> str >> str2;
+			result = obj.IsAnagrams(str, str2) ? "They are anagrams." : "They are not anagrams.";
+		default:
+			break;
+	}
 
+	cout << result << endl;
+}
 
 int main()
 {
-	/*
-	string str;
-	cout << "Input string to determine is the string has all unique characters." << endl;
-	cin >> str;
-
-	ArraysAndStrings obj;
-	string result = obj.IsUniqueCharacter(str) ? "The string has all unique characters." : "The string has duplicated characters.";
-	cout << result << endl;
-	*/
-
-	/*
-	string str;
-	cout << "Input string as c-style and reverse it." << endl;
-	cin >> str;
-
-	ArraysAndStrings obj;
-	char* result = obj.ReverseCStr(const_cast<char*>(str.c_str()));
-	//char* result = obj.AdvancedRreverse(const_cast<char*>(str.c_str()));
-	cout << result << endl;
-	*/
-
-	/*
-	string str;
-	cout << "Remove duplicated character in the input string." << endl;
-	cin >> str;
-
-	ArraysAndStrings obj;
-	string result = obj.RemoveDuplicatedChar(str);
-	cout << result << endl;
-	*/
-
-	string str1, str2;
-	cout << "Is the input two strings are anagrams?" << endl;
-	cin >> str1 >> str2;
-
-	ArraysAndStrings obj;
-	string result = obj.IsAnagrams(str1, str2) ? "They are anagrams." : "They are not anagrams.";
-	cout << result << endl;
-
-
+	CallArraysAndStrings(2);
     return 0;
 }
 
